74fc531efd51c94812bc168fd740d5b36baa9628
mahl <robertolopezt75@gmail.com>
2024-01-29 22:24:55 +0100

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,14 @@ describe('AppComponent', () => {
   it(`should have the 'E08-0BasicExercices-Routing' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('E08-0BasicExercices-Routing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, E08-0BasicExercices-Routing');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, E08-0BasicExercices-Routing',
+    );
   });
 });
